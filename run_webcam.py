e8b9de32a8caf9be4993568ec45fb52fd2770213
gamcoh <cohengamliel8@gmail.com>
2020-04-27 14:56:54 +0200

    Refactored run_webcam.py

--- run_webcam.py
+++ run_webcam.py
@@ -1,43 +1,45 @@
-import argparse
-import logging
-import time
-
-import cv2
-import numpy as np
-
-from tf_pose.estimator import TfPoseEstimator
-from tf_pose.networks import get_graph_path, model_wh
-
-logger = logging.getLogger('TfPoseEstimator-WebCam')
-logger.setLevel(logging.DEBUG)
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-formatter = logging.Formatter('[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s')
-ch.setFormatter(formatter)
-logger.addHandler(ch)
-
-fps_time = 0
-
 def str2bool(v):
+    if type(v) == bool:
+        return v
     return v.lower() in ("yes", "true", "t", "1")
 
-
 if __name__ == '__main__':
+    import argparse
+
     parser = argparse.ArgumentParser(description='tf-pose-estimation realtime webcam')
-    parser.add_argument('--camera', type=int, default=0)
+    parser.add_argument('--camera', type=int, default=0, help='A filename or a device for the camera input stream. default=0')
 
     parser.add_argument('--resize', type=str, default='0x0',
                         help='if provided, resize images before they are processed. default=0x0, Recommends : 432x368 or 656x368 or 1312x736 ')
     parser.add_argument('--resize-out-ratio', type=float, default=4.0,
                         help='if provided, resize heatmaps before they are post-processed. default=1.0')
 
-    parser.add_argument('--model', type=str, default='mobilenet_thin', help='cmu / mobilenet_thin / mobilenet_v2_large / mobilenet_v2_small')
+    parser.add_argument('--model', type=str, default='mobilenet_thin', help='cmu / mobilenet_thin / mobilenet_v2_large / mobilenet_v2_small. default=mobilenet_thin')
     parser.add_argument('--show-process', type=bool, default=False,
-                        help='for debug purpose, if enabled, speed for inference is dropped.')
+                        help='for debug purpose, if enabled, speed for inference is dropped. default=False')
     
-    parser.add_argument('--tensorrt', type=str, default="False",
-                        help='for tensorrt process.')
+    parser.add_argument('--tensorrt', type=bool, default=False,
+                        help='for tensorrt process. default=False')
     args = parser.parse_args()
+
+    import logging
+    import time
+
+    import cv2
+    import numpy as np
+
+    from tf_pose.estimator import TfPoseEstimator
+    from tf_pose.networks import get_graph_path, model_wh
+
+    logger = logging.getLogger('TfPoseEstimator-WebCam')
+    logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+  
+    fps_time = 0
 
     logger.debug('initialization %s : %s' % (args.model, get_graph_path(args.model)))
     w, h = model_wh(args.resize)
@@ -52,6 +54,9 @@ if __name__ == '__main__':
 
     while True:
         ret_val, image = cam.read()
+        if not ret_val:
+            logger.error('Something went wrong we the camera.')
+            break
 
         logger.debug('image process+')
         humans = e.inference(image, resize_to_default=(w > 0 and h > 0), upsample_size=args.resize_out_ratio)
@@ -62,7 +67,7 @@ if __name__ == '__main__':
         logger.debug('show+')
         cv2.putText(image,
                     "FPS: %f" % (1.0 / (time.time() - fps_time)),
-                    (10, 10),  cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                    (10, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
                     (0, 255, 0), 2)
         cv2.imshow('tf-pose-estimation result', image)
         fps_time = time.time()
